f7a0a62ae73614122a18566c706896069fd31400
munir45 <memamy3@uw.edu>
2025-12-02 22:10:03 -0800

    added normalization

--- BM25.py
+++ BM25.py
@@ -1,11 +1,7 @@
-#pip install rank-bm25
-
 import re
 import numpy as np
-# from rank_bm25 import BM25Okapi
 import os, pyterrier as pt, subprocess
 import pandas as pd
-
 
 # uncomment these lines to run on a virtual venv environment. there is a strange pyterrier version mismatch
 # and these lines take care of that.
@@ -48,84 +44,3 @@ search = input("Search: ")
 
 bm25_results = bm25.search(search)
 print(bm25_results)
-
-# def simple_tokenize(text: str):
-#     """
-#     Tokenize text
-#     """
-#     text = text.lower()
-#     tokens = re.split(r"[^a-zA-Z]+", text)
-#     return [t for t in tokens if t]
-
-# def build_bm25_corpus(recipes):
-#     """
-#     Build a corpus for BM25: each element is a list of tokens.
-#     Simultaneously return a list of recipe_ids for easy lookup later.
-#     """
-#     corpus = []
-#     recipe_ids = []
-
-#     for r in recipes:
-#         title_tokens = simple_tokenize(r["title"])
-#         ing_tokens = simple_tokenize(r.get("ingredients", ""))
-#         instr_tokens = simple_tokenize(r.get("instructions", ""))
-
-#         doc_tokens = (
-#             title_tokens * 3 +   # title (first important)
-#             ing_tokens * 2 +     # ingredients (Second important)
-#             instr_tokens         # instructions (normal)
-#         )
-
-#         corpus.append(doc_tokens)
-#         recipe_ids.append(r["id"])
-
-#         return corpus, recipe_ids
-
-
-# class BM25Engine:
-#     def __init__(self, recipes):
-#         """
-#         initialize BM25 from recipes list
-#         """
-#         self.recipes = recipes
-#         self.corpus,self.recipe_ids = build_bm25_corpus(recipes)
-#         self.bm25 = BM25Okapi(self.corpus) # feed all recipes text into BM 25
-
-#     def search (self, query:str, top_k:int = 20):
-#         """
-#         Have the user enter a query, and we return a sorted list of
-#         recipe results (including title, rating, recipe content, etc.).
-#         """
-#         tokens = simple_tokenize(query)
-#         scores = self.bm25.get_score(tokens) # For each recipe in the corpus, compute the BM25 relevance score.
-#         top_k = min(top_k, len(scores))
-#         top_idx = np.argsort(scores)[::-1][:top_k] # from small to big to big to small
-
-#         results = []
-#         for idx in top_idx:
-#             r = self.recipes[idx]
-#             results.append({
-#                 "recipes_id": self.recipes_ids[idx],
-#                 "title": r["title"],
-#                 "score": float (score(idx)),
-#                 "recipes": r
-#             })
-
-#         return results
-
-#     #For Hybrid (BM25 + Embedding) fusion, return "the cleanest, most direct BM25 output".
-#     #Do not return titles, recipe content, or dictionaries; return only pure index + score.
-#     #Hybrid search first retrieves index + score from BM25, then fuses it with embedding scores.
-
-#     def search_raw(self, query: str, top_k: int = 100):
-#         tokens = simple_tokenize(query)
-#         scores = self.bm25.get_scores(tokens)
-
-#         #rank from bigest to smallest
-#         top_k = min(top_k, len(scores))
-#         top_idx = np.argsort(scores)[::-1][:top_k]
-
-#         return top_idx, scores[top_idx]
-
-
-
---
+++ normalize.py
@@ -0,0 +1,69 @@
+import pandas as pd
+import numpy as np
+import pyterrier as pt
+import subprocess
+import os
+from sklearn.preprocessing import MinMaxScaler
+import embedding
+
+
+java_home = subprocess.check_output(['/usr/libexec/java_home', '-v', '17']).decode('utf-8').strip()
+os.environ["JAVA_HOME"] = java_home
+os.environ["JVM_PATH"]  = java_home + "/lib/server/libjvm.dylib"
+os.environ["PATH"]      = java_home + "/bin:" + os.environ["PATH"]
+
+if not pt.java.started():
+    pt.java.init()
+
+df = pd.read_csv("cleaned_output.csv")
+df['docno'] = df.index.astype(str)
+
+index_path = os.path.expanduser("./assets/pp_index")
+index_ref = pt.IndexRef.of(index_path + "/data.properties")
+bm25 = pt.BatchRetrieve(index_ref, wmodel="BM25", controls={"bm25.b": 0.75, "bm25.k_1": 1.0})
+
+def bm25_scores(query):
+    output = bm25.search(query)
+    output = output[["docno", "score"]]
+    output.columns = ["docno", "bm25_score"]
+    return output
+
+def normalize_bm25(df):
+    scaler = MinMaxScaler()
+    df['bm25_norm'] = scaler.fit_transform(df[['bm25_score']])
+    return df
+
+def embedding_scores(query):
+    results = embedding.search_embeddings(query)
+    embedding_df = pd.DataFrame(results)
+    embedding_df['docno'] = embedding_df['corpus_id'].astype(str)
+
+    embedding_df = embedding_df.rename(columns={"score": "vector_score"})
+    return embedding_df[["docno", "vector_score"]]
+
+def normalize_embedding(df):
+    scaler = MinMaxScaler()
+    df['vector_norm'] = scaler.fit_transform(df[["vector_score"]])
+    return df
+
+print(normalize_embedding(embedding_scores("Kidney bean curry")))
+
+def hybrid_search(query, b=0.5):
+    bm25_df = bm25_scores(query)
+    bm25_df = normalize_bm25(bm25_df)
+
+    vector_df = embedding_scores(query)
+    vector_df = normalize_embedding(vector_df)
+    
+    # note that we do a left join, effectively keeping all values of noramlized BM-25
+    # recall that BM25 values will not appear if the query term doesn't even exist in the recipe
+    # and to maintain the integrity of a 'hybrid' model, we must have some BM25 value to keep the
+    # recommendations 'hybrid', otherwise the embeddings will fully take over.
+    merged_df = pd.merge(bm25_df, vector_df, on="docno", how="left")
+
+    # b controls the weightage towards the final score. when b reaches 1 score relies on embedding, when 
+    # b reaches zero score relies on bm-25. final_score = ((1 - b) * bm_25 + (vector * b))
+    merged_df["hybrid_score"] = ((1-b) * merged_df['bm25_norm']) + (merged_df["vector_norm"] * b)
+    return merged_df.sort_values("hybrid_score", ascending=False)
+
+print(hybrid_search("a hot meal for a cold day"))
